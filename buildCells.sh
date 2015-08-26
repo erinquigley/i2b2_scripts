02776d9f1ca5e9c0f858abe88020a282a4b85a17
erinquigley <bmic@BMICs-Air.wlan.musc.edu>
2015-08-26 13:26:33 -0400

    changes

--- buildCells.sh
+++ buildCells.sh
@@ -1,26 +1,26 @@
-#!/bin/bash
-
+source ~/i2b2-variables.rc
+source ./i2b2-variables.rc
 ###########
 echo "[./buildCells.sh] build cells"
 ###########
 echo "[./buildCells.sh] Build Server-common"
-cd /opt/i2b2/source/edu.harvard.i2b2.server-common
-/opt/i2b2/ant/bin/ant clean dist deploy jboss_pre_deployment_setup
+cd $SERVER_SRC_DIR
+$ANT_HOME/ant clean dist deploy jboss_pre_deployment_setup
 echo "[./buildCells.sh] build PM Cell"
-cd ../edu.harvard.i2b2.pm
-/opt/i2b2/ant/bin/ant -f master_build.xml clean build-all deploy
+cd $PM_SRC_DIR
+$ANT_HOME/ant -f master_build.xml clean build-all deploy
 echo "[./buildCells.sh] Build Ontology Cell"
-cd /opt/i2b2/source/edu.harvard.i2b2.ontology
-/opt/i2b2/ant/bin/ant -f master_build.xml clean build-all deploy
+cd $ONT_SRC_DIR
+$ANT_HOME/ant -f master_build.xml clean build-all deploy
 echo "[./buildCells.sh] Build CRC Cell"
-cd ../edu.harvard.i2b2.crc
-/opt/i2b2/ant/bin/ant -f master_build.xml clean build-all deploy
+cd $CRC_SRC_DIR
+$ANT_HOME/ant -f master_build.xml clean build-all deploy
 echo "[./buildCells.sh] Build IM Cell"
-cd ../edu.harvard.i2b2.im
-/opt/i2b2/ant/bin/ant -f master_build.xml clean build-all deploy
+cd $IM_SRC_DIR
+$ANT_HOME/ant -f master_build.xml clean build-all deploy
 echo "[./buildCells.sh] Build Workplace Cell"
-cd ../edu.harvard.i2b2.workplace
-/opt/i2b2/ant/bin/ant -f master_build.xml clean build-all deploy
+cd $WORK_SRC_DIR
+$ANT_HOME/ant -f master_build.xml clean build-all deploy
 echo "[./buildCells.sh] Build File Repository Cell"
-cd ../edu.harvard.i2b2.fr
-/opt/i2b2/ant/bin/ant -f master_build.xml clean build-all deploy
+cd $FR_SRC_DIR
+$ANT_HOME/ant -f master_build.xml clean build-all deploy
--- skel/i2b2_config_data.js
+++ skel/i2b2_config_data.js
@@ -6,14 +6,14 @@
 	lstDomains: [
 		{ domain: "i2b2demo",
 		  name: "Data Analysis/DataMart",
-		  urlCellPM: "http://"I2B2_HOME"/i2b2/services/PMService/",
+		  urlCellPM: "http://I2B2_URL/i2b2/services/PMService/",
 		  allowAnalysis: true,
 		  debug: false,
 		  adminOnly: false
 		},
 		{ domain: "i2b2demo",
 		  name: "Administration",
-		  urlCellPM: "http://"I2B2_HOME"/i2b2/services/PMService/",
+		  urlCellPM: "http://I2B2_URL/i2b2/services/PMService/",
 		  allowAnalysis: true,
 		  debug: false,
 		  adminOnly: true
