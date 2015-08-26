8044c0e168ba1136cced92698a3553fea30c40d4
erinquigley <bmic@BMICs-Air.wlan.musc.edu>
2015-08-26 14:27:08 -0400

    commit

--- buildCells.sh
+++ buildCells.sh
@@ -5,22 +5,22 @@ echo "[./buildCells.sh] build cells"
 ###########
 echo "[./buildCells.sh] Build Server-common"
 cd $SERVER_SRC_DIR
-$ANT_HOME/ant clean dist deploy jboss_pre_deployment_setup
+$ANT_HOME/bin/ant clean dist deploy jboss_pre_deployment_setup
 echo "[./buildCells.sh] build PM Cell"
 cd $PM_SRC_DIR
-$ANT_HOME/ant -f master_build.xml clean build-all deploy
+$ANT_HOME/bin/ant -f master_build.xml clean build-all deploy
 echo "[./buildCells.sh] Build Ontology Cell"
 cd $ONT_SRC_DIR
-$ANT_HOME/ant -f master_build.xml clean build-all deploy
+$ANT_HOME/bin/ant -f master_build.xml clean build-all deploy
 echo "[./buildCells.sh] Build CRC Cell"
 cd $CRC_SRC_DIR
-$ANT_HOME/ant -f master_build.xml clean build-all deploy
+$ANT_HOME/bin/ant -f master_build.xml clean build-all deploy
 echo "[./buildCells.sh] Build IM Cell"
 cd $IM_SRC_DIR
-$ANT_HOME/ant -f master_build.xml clean build-all deploy
+$ANT_HOME/bin/ant -f master_build.xml clean build-all deploy
 echo "[./buildCells.sh] Build Workplace Cell"
 cd $WORK_SRC_DIR
-$ANT_HOME/ant -f master_build.xml clean build-all deploy
+$ANT_HOME/bin/ant -f master_build.xml clean build-all deploy
 echo "[./buildCells.sh] Build File Repository Cell"
 cd $FR_SRC_DIR
-$ANT_HOME/ant -f master_build.xml clean build-all deploy
+$ANT_HOME/bin/ant -f master_build.xml clean build-all deploy
