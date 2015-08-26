7e161ee7468870ad746bc705bbcde8d23ba3df5f
erinquigley <bmic@BMICs-Air.wlan.musc.edu>
2015-08-26 14:37:08 -0400

    jboss

--- run_jboss.sh
+++ run_jboss.sh
@@ -2,5 +2,6 @@
 #########
 #Script Name: run_jboss.sh
 #########
+source ./i2b2-variables.rc
 #Start up JBoss and deploy i2b2.war, ojdbc.jar and the I2B2 cells
-nohup $JBOSS_HOME/bin/standalone.sh -b 0.0.0.0
+nohup $JBOSS_HOME/bin/standalone.sh -b 0.0.0.0 &
